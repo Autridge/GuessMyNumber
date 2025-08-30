b190df2dcd16433cd1264788617bd0a45fbea5c7
Autridge B. Morris <talktoautridge@gmail.com>
2025-08-30 12:13:22 +0200

    feat: generating random num and restart functionalities

--- script.js
+++ script.js
@@ -3,34 +3,77 @@ let levels = document.getElementById("levels");
 let selectedLevel = levels.value; // Should contain first value "Rookie" by default
 let attempts = 0;
 
-levels.addEventListener("change", function(){
-    selectedLevel = levels.value; // Should contain new value once level is changed
-})
+let score = 20;
+let highScore = 0;
 
-//Getting User Input 
-let userInput = document.querySelector(".guess"); 
-let userNumber = userInput.value; //get the input when page loads (likely to be "")
+let randomNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(randomNumber);
+localStorage.setItem("randomNum", JSON.stringify(randomNumber));
+
+levels.addEventListener("change", function () {
+  selectedLevel = levels.value; // Should contain new value once level is changed
+});
+
+//displaying Message
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
+//Getting User Input
+let userInput = document.querySelector(".guess");
 let checkBtn = document.querySelector(".check");
 
-checkBtn.addEventListener("click", ()=>{ 
-    userNumber = userInput.value; //making sure you have the updated value when user clicks "check"
-})
+checkBtn.addEventListener("click", () => {
+  let userNumber = Number(userInput.value); //making sure you have the updated value when user clicks "check"
+
+  if (!userNumber) {
+    displayMessage("⛔ No number!");
+  } else if (userNumber === randomNumber) {
+    displayMessage("🎉 Correct Number!");
+    document.querySelector(".number").textContent = randomNumber;
+    document.querySelector("body").style.background =
+      "url(images/success-screen.png) center/cover no-repeat";
+    document.querySelector(".number").style.width = "30rem";
+    document.querySelector(".guess-head").style.color = "#0eee7e";
+    userInput.style.color = "#0eee7e";
+    userInput.style.borderColor = "#0eee7e";
+    checkBtn.style.display = "none";
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
+  } else if (userNumber !== randomNumber) {
+    if (score > 1) {
+      displayMessage(
+        userNumber > randomNumber ? "📈 Too high!" : "📉 Too low!"
+      );
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("💥 You lost the game!");
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
 
 //Generating Random Number
 let restartBtn = document.querySelector(".right button:last-of-type");
 
-restartBtn.addEventListener("click", ()=>{  //generate new number every time the button is clicked
-    let randomNumber = Math.floor(Math.random() * (50)) + 1; // 1 - 50 default range 
-    localStorage.setItem("randomNum", JSON.stringify(randomNumber));
-})
-
-
-
-
-
-
-
-
-
-
-
+restartBtn.addEventListener("click", () => {
+  //generate new number every time the button is clicked
+  randomNumber = Math.floor(Math.random() * 20) + 1; // 1 - 20 default range
+  localStorage.setItem("randomNum", JSON.stringify(randomNumber));
+  displayMessage("Start guessing...");
+  document.querySelector(".number").textContent = "?";
+  document.querySelector("body").style.background =
+    "url(images/Screen--Playing-bkg.png) center/cover no-repeat";
+  document.querySelector(".number").style.width = "30rem";
+  document.querySelector(".guess-head").style.color = "#e7819b";
+  userInput.style.color = "#e7819b";
+  userInput.style.borderColor = "none";
+  checkBtn.style.display = "flex";
+  userInput.value = "";
+  score = 20;
+  document.querySelector(".score").textContent = score;
+});
